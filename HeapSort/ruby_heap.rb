beaf497ab7ea06ab03548936ad3cad917d303e0f
Malcolm Ahoy <colmahoy@gmail.com>
2014-05-01 22:25:24 -0700

    Refactor Ruby Heap Sort

--- HeapSort/ruby_heap.rb
+++ HeapSort/ruby_heap.rb
@@ -1,5 +1,29 @@
-def heap_sort(arr, count)
+def heapify(arr, count)
   ((count - 2) / 2).downto(0) {|start| sift_down(arr, start, count - 1) }
+end
+
+def sift_down(arr, start_val, end_val)
+  root = start_val
+  
+  while root * 2 + 1 <= end_val
+    child = root * 2 + 1
+    break if child > end_val
+    
+    if child + 1 <= end_val && arr[child] < arr[child + 1]
+      child += 1
+    end
+    
+    if arr[root] < arr[child]
+      arr[root], arr[child] = arr[child], arr[root]
+      root = child
+    else
+      return
+    end
+  end
+end
+
+def heap_sort(arr, count)
+  heapify(arr, count)
   
   (count - 1).downto(1) do |end_val|
     arr[end_val], arr[0] = arr[0], arr[end_val]
@@ -7,25 +31,6 @@ def heap_sort(arr, count)
   end
   
   arr
-end
-
-def sift_down(arr, start_val, end_val)
-  root = start_val
-  
-  loop do
-    child = root * 2 + 1
-    break if child > end_val
-    
-    if child + 1 <= end_val && arr[child] < arr[child + 1]
-      child += 1
-    end
-    if arr[root] < arr[child]
-      arr[root], arr[child] = arr[child], arr[root]
-      root = child
-    else
-      break
-    end
-  end
 end
 
 p heap_sort([1,3,5,7,9,8,6,4,2], 9)
