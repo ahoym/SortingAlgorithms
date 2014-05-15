cd2741e07a9a59a402be53f54f623cb1ef5f266e
Malcolm Ahoy <colmahoy@gmail.com>
2014-05-15 13:07:12 -0700

    Added new Ruby/JS/Python Quick Sorts. Use arrays to hold values instead of being pointer based.

--- QuickSort/py_quick.py
+++ QuickSort/py_quick.py
@@ -1,4 +1,25 @@
-def quick_sort(arr, left, right):
+def quick_sortI(arr):
+  if len(arr) <= 1:
+    return arr;
+    
+  left = []
+  eq = []
+  right = []
+  pivot = arr[0]
+  
+  for el in arr:
+    if el < pivot:
+      left.append(el)
+    elif el == pivot:
+      eq.append(el)
+    else:
+      right.append(el)
+  
+  return quick_sortI(left) + eq + quick_sortI(right);
+
+
+# =================== Pointer Based =======================
+def quick_sortII(arr, left, right):
   if len(arr) < 2:
     return arr;
     
@@ -26,6 +47,6 @@ def partition(arr, left, right, pivot_index):
   arr[store_index], arr[right] = arr[right], arr[store_index]
   return store_index;
   
-print quick_sort([1,3,5,7,9,8,6,4,2,0], 0, 9)
-print quick_sort([9,8,7,6,5,4,3,2,1,0], 0, 9)
-print quick_sort([1,2,3,4,5,0,4,3,2,1], 0, 9)
+print quick_sortI([1,3,5,7,9,8,6,4,2,0])
+print quick_sortI([9,8,7,6,5,4,3,2,1,0])
+print quick_sortI([1,2,3,4,5,0,4,3,2,1])
