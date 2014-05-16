f4cb772770056d87128830e99db329441dc4a6af
Malcolm Ahoy <colmahoy@gmail.com>
2014-05-16 16:15:34 -0700

    Rearrange Ruby RSpec Bubble Sort Tests into a more fluid order, added additional tests

--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -9,6 +9,17 @@ describe "#bubble_sort" do
   it "should sort an unsorted array" do 
     expect(bubble_sort(arr)).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     expect(bubble_sort(random_arr)).to eq(sorted_random_arr)
+    expect(bubble_sort([9,8,7,6,5,4,3,2,1,0])).to eq([0,1,2,3,4,5,6,7,8,9])
+    expect(bubble_sort([1,2,3,4,5,0,4,3,2,1])).to eq([0,1,1,2,2,3,3,4,4,5])
+  end
+  
+  it "should handle small and simple unsorted arrays" do
+    expect(bubble_sort([3, 2, 1])).to eq([1, 2, 3])
+    expect(bubble_sort([2, 1])).to eq([1, 2])
+  end
+  
+  it "should handle large unsorted arrays" do
+    expect(bubble_sort(huge_random_arr)).to eq(huge_random_arr.sort)
   end
   
   it "should not alter an already sorted array" do
@@ -20,11 +31,7 @@ describe "#bubble_sort" do
     expect(bubble_sort([])).to eq([])
   end
   
-  it "should handle small and simple unsorted arrays" do
-    expect(bubble_sort([3, 2, 1])).to eq([1, 2, 3])
-  end
-  
-  it "should handle large unsorted arrays" do
-    expect(bubble_sort(huge_random_arr)).to eq(huge_random_arr.sort)
+  it "should handle single element arrays" do
+    expect(bubble_sort([1])).to eq([1])
   end
 end
