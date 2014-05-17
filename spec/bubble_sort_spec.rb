da4072cefe7eb38f4c1ed25e031404222ddf241b
Malcolm Ahoy <colmahoy@gmail.com>
2014-05-17 11:00:22 -0700

    Rearranged tests so that the order is more sensible and fluid

--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -3,14 +3,22 @@ require_relative '../BubbleSort/ruby_bubble'
 describe "#bubble_sort" do
   let(:arr) { [1, 3, 5, 7, 9, 8, 6, 4, 2, 0] }
   let(:random_arr) { Array.new(10) { rand(100) } }
-  let(:huge_random_arr) { Array.new(10000) { rand(100) } }
   let(:sorted_random_arr) { random_arr.sort }
+  let(:huge_random_arr) { Array.new(10000) { rand(100) } }
   
   it "should sort an unsorted array" do 
     expect(bubble_sort(arr)).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     expect(bubble_sort(random_arr)).to eq(sorted_random_arr)
     expect(bubble_sort([9,8,7,6,5,4,3,2,1,0])).to eq([0,1,2,3,4,5,6,7,8,9])
     expect(bubble_sort([1,2,3,4,5,0,4,3,2,1])).to eq([0,1,1,2,2,3,3,4,4,5])
+  end
+  
+  it "should return an empty array, when given one" do
+    expect(bubble_sort([])).to eq([])
+  end
+  
+  it "should handle single element arrays" do
+    expect(bubble_sort([1])).to eq([1])
   end
   
   it "should handle small and simple unsorted arrays" do
@@ -25,13 +33,5 @@ describe "#bubble_sort" do
   it "should not alter an already sorted array" do
     expect(bubble_sort(sorted_random_arr)).to eq(sorted_random_arr)
     expect(bubble_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-  end
-  
-  it "should return an empty array, when given one" do
-    expect(bubble_sort([])).to eq([])
-  end
-  
-  it "should handle single element arrays" do
-    expect(bubble_sort([1])).to eq([1])
   end
 end
--- spec/insertion_sort_spec.rb
+++ spec/insertion_sort_spec.rb
@@ -3,8 +3,8 @@ require_relative '../InsertionSort/ruby_insertion'
 describe "#insertion_sort" do
   let(:arr) { [1, 3, 5, 7, 9, 8, 6, 4, 2, 0] }
   let(:random_arr) { Array.new(10) { rand(100) } }
-  let(:huge_random_arr) { Array.new(10000) { rand(100) } }
   let(:sorted_random_arr) { random_arr.sort }
+  let(:huge_random_arr) { Array.new(10000) { rand(100) } }
   
   it "should sort an unsorted array" do
     expect(insertion_sort(arr)).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
